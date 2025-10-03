7362d8d2cf16633802010b01bf04eb118c6c591f
Sephanne Flor <sephanneflor9@gmail.com>
2025-10-02 23:41:46 -0700

    Create Strange Counter.cs

---
+++ Strange Counter.cs
@@ -0,0 +1,57 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'strangeCounter' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts LONG_INTEGER t as parameter.
+     */
+
+    public static long strangeCounter(long t)
+    {
+        long start = 3;
+        long time = 1;
+
+       
+        while (time + start - 1 < t)
+        {
+            time += start;
+            start *= 2;
+        }
+
+        
+        return start - (t - time);
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        long t = Convert.ToInt64(Console.ReadLine().Trim());
+
+        long result = Result.strangeCounter(t);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
